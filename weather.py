eb6cdf3cc2c305cd3038097e0c2af9a457bcc7bf
XercoDylan <xerco@mit.edu>
2025-08-13 12:59:16 -0400

    Done

---
+++ weather.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+
+def get_current_weather(city="Upper Marlboro"):
+    requests_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    weather_data = requests.get(requests_url).json()
+    return weather_data
